c875e9e302293dd17c154d9384df01534f8e1695
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-06-29 14:59:12 +0100

    refactored code

--- lib/app/overview.dart
+++ lib/app/overview.dart
@@ -1,4 +1,9 @@
+import 'package:benji_vendor/theme/colors.dart';
 import 'package:flutter/material.dart';
+
+import 'dashboard/dashboard.dart';
+import 'product/product.dart';
+import 'profile/profile.dart';
 
 class OverView extends StatefulWidget {
   const OverView({super.key});
@@ -8,10 +13,31 @@ class OverView extends StatefulWidget {
 }
 
 class _OverViewState extends State<OverView> {
+  int _currentIndex = 1;
+
+  final List<Widget> _pages = const [
+    Dashboard(),
+    Product(),
+    Profile(),
+  ];
+
+  void onTappedBar(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: _pages[_currentIndex],
+      backgroundColor: kPrimaryColor,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: kPrimaryColor,
+        elevation: 10.0,
+        currentIndex: _currentIndex,
+        items: const [],
+      ),
     );
   }
 }
