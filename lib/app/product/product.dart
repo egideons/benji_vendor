a36c880c83e77f1a89acc5e2cb774cd4b7dcb2f7
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-06-29 14:50:30 +0100

    created new file

---
+++ lib/app/product/product.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Product extends StatefulWidget {
+  const Product({super.key});
+
+  @override
+  State<Product> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Product> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
