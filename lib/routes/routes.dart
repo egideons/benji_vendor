6bdc2260f1e9a629a7e4a3ac8359cfde68e6480f
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-06-29 09:36:40 +0100

    Started the project - created new folders, refactored code.

---
+++ lib/routes/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Map<String, WidgetBuilder> routes = {
+  //Splash Screens
+  // StartupSplashscreen.routeName: (context) => const StartupSplashscreen(),
+};
---
+++ lib/theme/colors.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+//App Main Colors
+Color kPrimaryColor = const Color(
+  0xFFFFFFFF,
+);
+Color kSecondaryColor = HexColor(
+  "#0003C4",
+);
+Color kAccentColor = Color(
+  0xFFEC2623,
+);
+
+Color KLoadingColor = Color(
+  0xFFFFA927,
+);
+
+const Color kGreyColor1 = Color(
+  0xFFA5A5A5,
+);
+Color kGreyColor2 = Color(
+  0xFF757575,
+);
+const Color kBlackColor = Color(
+  0xFF000000,
+);
+const Color kDarkGreyColor = Colors.black12;
+
+//Background Colors
+const Color kRedColor = Color(
+  0xFFE74C3C,
+);
+const Color kErrorColor = Color(
+  0xFFE74C3C,
+);
+const Color kSuccessColor = Color(
+  0xFF00D220,
+);
+
+const Color kTransparentColor = Color(
+  0x00000000,
+);
+
+//Text Colors
+const Color kTextWhiteColor = Color(
+  0xFFFFFFFF,
+);
+const Color kTextBlackColor = Color.fromARGB(
+  255,
+  0,
+  0,
+  0,
+);
+Color kTextGreyColor = Color(
+  0xFF7E8A97,
+);
+const Color kBlueLinkTextColor = Color(
+  0xFF2196F3,
+);
+//Icon Colors
+const Color kIconLightColor = Color(
+  0xFFA5A5A5,
+);
+
+//Border Colors
+const Color kBorderLightColor = Color(
+  0xFFA5A5A5,
+);
+const Color kErrorBorderColor = Color(
+  0xFFE74C3C,
+);
