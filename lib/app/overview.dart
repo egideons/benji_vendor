a9c3fae9e038eaa1396815eaa7500ebb77f28790
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-06-29 14:35:40 +0100

    CREATED NEW FILES AND ANIMATIONS - new files:     lib/app/overview.dart     lib/splash screens/login splash screen.dart     lib/splash screens/startup splash screen.dart - new assets:     no-internet.gif     registration-successful.gif     successful.gif

---
+++ lib/app/overview.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OverView extends StatefulWidget {
+  const OverView({super.key});
+
+  @override
+  State<OverView> createState() => _OverViewState();
+}
+
+class _OverViewState extends State<OverView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
---
+++ lib/splash screens/startup splash screen.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: camel_case_types, file_names
+
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import '../app/overview.dart';
+import '../theme/colors.dart';
+import '../theme/constants.dart';
+
+class StartupSplashscreen extends StatefulWidget {
+  static String routeName = "Startup Splash Screen";
+  const StartupSplashscreen({super.key});
+
+  @override
+  State<StartupSplashscreen> createState() => _StartupSplashscreenState();
+}
+
+class _StartupSplashscreenState extends State<StartupSplashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(
+        const Duration(
+          seconds: 4,
+        ), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (ctx) => const OverView(),
+        ),
+      );
+    });
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 4,
+              width: MediaQuery.of(context).size.width / 2,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    "assets/images/splash screen/frame-1.png",
+                  ),
+                ),
+              ),
+            ),
+            kSizedBox,
+            SpinKitThreeInOut(
+              color: kSecondaryColor,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
