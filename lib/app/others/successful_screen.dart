ccee2c6d1b375c8a222cc5d496274228f790978f
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-07-07 15:29:27 +0100

    created a reusable screen

---
+++ lib/app/others/successful_screen.dart
@@ -0,0 +1,87 @@
+// ignore_for_file: camel_case_types, file_names
+
+import 'package:flutter/material.dart';
+
+import '../../providers/constants.dart';
+import '../../reusable widgets/my elevatedButton.dart';
+
+class SuccessfulScreen extends StatelessWidget {
+  final String text;
+  final String elevatedButtonTitle;
+  const SuccessfulScreen({
+    super.key,
+    required this.text,
+    required this.elevatedButtonTitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        maintainBottomViewPadding: true,
+        child: Container(
+          margin: const EdgeInsets.only(
+            top: kDefaultPadding,
+            left: kDefaultPadding,
+            right: kDefaultPadding,
+          ),
+          child: ListView(
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            children: [
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        "assets/animations/successful/successful-payment.gif",
+                      ),
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: kDefaultPadding * 2,
+              ),
+              SizedBox(
+                width: 307,
+                child: Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Color(
+                      0xFF676565,
+                    ),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: -0.36,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.3,
+              ),
+              MyElevatedButton(
+                buttonTitle: elevatedButtonTitle,
+                elevation: 10.0,
+                titleFontSize: 18,
+                circularBorderRadius: 10,
+                maximumSizeHeight: 68,
+                maximumSizeWidth: MediaQuery.of(context).size.width,
+                minimumSizeHeight: 68,
+                minimumSizeWidth: MediaQuery.of(context).size.width,
+                onPressed: () {
+                  Navigator.of(context).pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
