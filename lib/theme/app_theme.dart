10305d6fbe3b71bedea29c33c3bc60f467ac6d2b
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-01-18 10:47:20 +0100

    adding the platform check for iOS and android in the main.dart file

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,11 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
+  static CupertinoThemeData iOSLightTheme = const CupertinoThemeData(
+    applyThemeToAll: true,
+    textTheme: CupertinoTextThemeData(),
+    brightness: Brightness.light,
+  );
+  static CupertinoThemeData iOSDarkTheme = const CupertinoThemeData(
+    textTheme: CupertinoTextThemeData(),
+    applyThemeToAll: true,
+    brightness: Brightness.dark,
+  );
+
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
-      brightness: Brightness.light, textTheme: GoogleFonts.senTextTheme());
+    brightness: Brightness.light,
+    textTheme: GoogleFonts.senTextTheme(),
+  );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
-      brightness: Brightness.dark, textTheme: GoogleFonts.senTextTheme());
+    brightness: Brightness.dark,
+    textTheme: GoogleFonts.senTextTheme(),
+  );
 }
